8f6ec63157e92392195c678974ce1f8d553580cb
Danro <86835117+rsnlnd@users.noreply.github.com>
2026-02-20 12:17:56 +0100

    Add project files.

---
+++ SIBDAT25-2S-Databaser-CCDL/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Npgsql;
+
+namespace SIBDAT25_2S_DBString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Programmet tester kun om der kan oprettes forbindelse til databasen.
+            // Ingen læse-/skriveoperationer udføres her.
+            // NOTE: I produktion bør du hente URI/password fra en sikker kilde (fx miljøvariabel).
+
+            var uri = new Uri("postgres://postgres:FSP02UXAG14HBHLiqtjKMU7R47akAG2Hk0Lsh0ySg2DBO0OfkHLkvwp8WOoXx89u@95.211.27.223:5510/postgres");
+
+            // Byg en Npgsql-tilslutningsstreng ud fra URI'en
+            var builder = new NpgsqlConnectionStringBuilder
+            {
+                Host = uri.Host,
+                Port = uri.Port,
+                Username = uri.UserInfo.Split(':')[0],
+                Password = uri.UserInfo.Split(':')[1],
+                Database = uri.AbsolutePath.TrimStart('/'),
+                SslMode = SslMode.Disable
+            };
+
+            string connectionString = builder.ConnectionString;
+
+            // Forsøg at åbne forbindelse — rapportér kun succes eller fejl
+            try
+            {
+                using (var conn = new NpgsqlConnection(connectionString))
+                {
+                    conn.Open();
+                    Console.WriteLine("Forbindelse oprettet succesfuldt.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Forbindelse fejlede: {ex.Message}");
+                Environment.ExitCode = 1;
+            }
+        }
+    }
+}
---
+++ SIBDAT25-2S-Databaser-CCDL/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SIBDAT25-2S-Databaser-CCDL")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SIBDAT25-2S-Databaser-CCDL")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6b3e3361-c8a0-4ed0-a7e8-b107e1168419")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
